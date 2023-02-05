01f250e6cd996cca8e4edea9ea269e0d5e39c817
Ilyukina <anyailyukina@yandex.ru>
2023-02-05 21:08:17 +0800

    получение данных из формы

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // replace the value below with the Telegram token you receive from @BotFather
 const token = "6034648637:AAF3KUN6NQsTVSd5W_npG4sLONbRNkTJrqY";
-const webAppUrl = 'https://yandex.ru'
+const webAppUrl = "https://stellar-salamander-baf63d.netlify.app";
 const TelegramBot = require("node-telegram-bot-api");
 
 // Create a bot that uses 'polling' to fetch new updates
@@ -32,17 +32,33 @@ bot.on("message", async (msg) => {
       "Привет, улыбнись и заполни форму!!!", {
         reply_markup: {
           keyboard: [
-            [{text: 'Заполнить форму'}]
+            [{text: 'Заполнить форму', web_app: { url: webAppUrl + '/form' }}]
           ]
         }
       }
     );
-    await bot.sendMessage(chatId, {
+    await bot.sendMessage(chatId, 'Заходите в наш интернет магазин', {
       reply_markup: {
         inline_keyboard: [
           [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
         ],
       },
     });
+  }
+  console.log(msg);
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+      console.log(data);
+      await bot.sendMessage(chatId, "Cпасибо за обратную связь!");
+      await bot.sendMessage(chatId, "Ваш город: " + data?.city);
+      await bot.sendMessage(chatId, "Ваша улица: " + data?.street);
+      setTimeout(async() => {
+        await bot.sendMessage(chatId, "Всю информацию вы получите в этом чате");
+      }, 3000)
+
+    } catch {
+      console.log(e)
+    }
   }
 });
